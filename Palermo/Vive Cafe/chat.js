abea59b2b6c39eb4b045c5c23d75a9a61d55c005
Cayetana Peces <mpecessanchezsaravi@itba.edu.ar>
2023-06-19 11:52:32 -0300

    ultimo

---
+++ Palermo/Vive Cafe/chat.js
@@ -0,0 +1,22 @@
+document.addEventListener("DOMContentLoaded", function() {
+  var chatContainer = document.getElementById("chat-container");
+  var messageInput = document.getElementById("message-input");
+  var sendButton = document.getElementById("send-button");
+
+  sendButton.addEventListener("click", function() {
+    var message = messageInput.value;
+    if (message !== "") {
+      sendMessage(message);
+      messageInput.value = "";
+    }
+  });
+
+  function sendMessage(message) {
+    var messageElement = document.createElement("p");
+    messageElement.classList.add("message", "from-user");
+    messageElement.innerText = message;
+    chatContainer.appendChild(messageElement);
+    
+    chatContainer.scrollTop = chatContainer.scrollHeight;
+  }
+});
